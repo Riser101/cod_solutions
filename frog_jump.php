2d87d7ed64883294b86877ff0bb1c9261f9fe368
yousuf syed <sy.yousuf9106@gmail.com>
2018-03-17 20:41:10 +0530

    corrected commit block

--- frog_jump.php
+++ frog_jump.php
@@ -1,6 +1,6 @@
 <?php
 
-/**
+/*
 	A small frog wants to get to the other side of the road. The frog is currently located at position X and wants to get to a position greater than or equal to Y. The small frog always jumps a fixed distance, D.
 
 	Count the minimal number of jumps that the small frog must perform to reach its target.
@@ -30,7 +30,7 @@
 	expected worst-case time complexity is O(1);
 	
 	expected worst-case space complexity is O(1).
-**/
+*/
 
 function solution($X, $Y, $D) {
 	return ceil((abs($X-$Y))/$D);
